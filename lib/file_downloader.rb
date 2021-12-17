2f103803477770f80d43511a53949a3c97ef420d
vergunda <vergunda@gmail.com>
2021-12-17 11:51:59 +0200

    add initializer module

--- lib/file_downloader.rb
+++ lib/file_downloader.rb
@@ -7,8 +7,9 @@ require_relative 'validator'
 
 module FileDownloader
 
-  extend Validator
   extend Constants::Defaults
+  extend Initializer
+  extend Validator
 
   class << self
 
@@ -112,31 +113,6 @@ module FileDownloader
 
     def logger
       @logger ||= Logger.new
-    end
-
-    def init_paths(path, downloads_path)
-      init_file_path(path)
-      init_download_path(downloads_path)
-    end
-
-    def init_file_path(path)
-      return unless path
-
-      @file_path = if File.exist? Constants::Defaults::ROOT + path
-                     Constants::Defaults::ROOT + path
-                   elsif File.exist? path
-                     path
-                   end
-    end
-
-    def init_download_path(path)
-      @downloads_path = if path && File.directory?("#{Constants::Defaults::ROOT}/#{path}")
-                          Constants::Defaults::ROOT + path
-                        elsif path && File.directory?(path)
-                          path
-                        elsif path.nil?
-                          Constants::Defaults::DOWNLOADS_PATH
-                        end
     end
 
     def meta_data_valid?(meta_data, url)
--- lib/initializer.rb
+++ lib/initializer.rb
@@ -3,3 +3,31 @@ require 'json'
 require 'i18n'
 
 I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
+
+module Initializer
+
+  def init_paths(path, downloads_path)
+    init_file_path(path)
+    init_download_path(downloads_path)
+  end
+
+  def init_file_path(path)
+    return unless path
+
+    @file_path = if File.exist? Constants::Defaults::ROOT + path
+                   Constants::Defaults::ROOT + path
+                 elsif File.exist? path
+                   path
+                 end
+  end
+
+  def init_download_path(path)
+    @downloads_path = if path && File.directory?("#{Constants::Defaults::ROOT}/#{path}")
+                        Constants::Defaults::ROOT + path
+                      elsif path && File.directory?(path)
+                        path
+                      elsif path.nil?
+                        Constants::Defaults::DOWNLOADS_PATH
+                      end
+  end
+end
